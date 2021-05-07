1d1c8e24f347329889e78f79b2894c5c7ac74c47
Delile Sebastien <sebastiendelile@gmail.com>
2021-05-07 20:04:35 +0200

    test: start newbill container file
    start some tests on the newbill container file
    lot of issues to fix, hard to test the event handler functions

--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -1,14 +1,143 @@
-import { screen } from "@testing-library/dom"
-import NewBillUI from "../views/NewBillUI.js"
-import NewBill from "../containers/NewBill.js"
+import { fireEvent, screen } from "@testing-library/dom";
+import userEvent from '@testing-library/user-event'
+import NewBillUI from "../views/NewBillUI.js";
+import NewBill from "../containers/NewBill.js";
+import { localStorageMock } from "../__mocks__/localStorage.js";
 
+//setup for tests
+const onNavigateOriginal = (pathname) => {
+  document.body.innerHTML = ROUTES({ pathname });
+};
+
+class InitiateNewBill {
+  constructor(onNavigateFunction = onNavigateOriginal) {
+    const onNavigate = onNavigateFunction;
+    Object.defineProperty(window, "localStorage", { value: localStorageMock });
+    window.localStorage.setItem(
+      "user",
+      JSON.stringify({
+        type: "Employee",
+        email: "a@a",
+      })
+    );
+    const html = NewBillUI();
+    document.body.innerHTML = html;
+    this.method = new NewBill({
+      document,
+      onNavigate,
+      firestore: null,
+      localStorage: window.localStorage,
+    });
+  }
+}
+
+class FillTheForm {
+  constructor({
+    type = "Restaurants et bars",
+    name = "Pot",
+    amount = "100",
+    date = "2021-05-01",
+    vat = "20",
+    pct = "20",
+    comment = "YOLO",
+    file = new File(["sample"], "sample.png", { type: "image/png" }),
+  } = {}) {
+    document.querySelector(`select[data-testid="expense-type"]`).value = type;
+    document.querySelector(`input[data-testid="expense-name"]`).value = name;
+    document.querySelector(`input[data-testid="amount"]`).value = amount;
+    document.querySelector(`input[data-testid="datepicker"]`).value = date;
+    document.querySelector(`input[data-testid="vat"]`).value = vat;
+    document.querySelector(`input[data-testid="pct"]`).value = pct;
+    document.querySelector(`textarea[data-testid="commentary"]`).value = comment;
+    Object.defineProperty(document.querySelector(`input[data-testid="file"]`), "files", {
+      value: [file],
+    });
+  }
+}
+//end of setup
 
 describe("Given I am connected as an employee", () => {
-  describe("When I am on NewBill Page", () => {
-    test("Then ...", () => {
-      const html = NewBillUI()
-      document.body.innerHTML = html
-      //to-do write assertion
-    })
+  describe("When I access NewBill Page", () => {
+    test("Then the newBill page should be rendered", () => {
+      new InitiateNewBill();
+      expect(screen.getAllByText("Envoyer une note de frais")).toBeTruthy();
+    });
+    test("Then a form with nine fields should be rendered", () => {
+      new InitiateNewBill();
+      const form = document.querySelector("form");
+      expect(form.length).toEqual(9);
+    });
+  });
+  describe("When I am on NewBill Page and I uncorrectly fill the form and submit(missing type)", () => {
+    test("Then the form should not been sent and the error should be highlighted", () => {
+      const newBill = new InitiateNewBill();
+      newBill.method.handleSubmit = jest.fn()
+      new FillTheForm()
+      userEvent.click(document.getElementById('btn-send-bill'))
+      expect(newBill.method.handleSubmit).toHaveBeenCalled();
+    });
   })
-})
+
+
+
+  describe("When I am on NewBill Page and I correctly fill the form and submit", () => {
+    test("Then the app should send the info to backend", () => {
+      //yolo
+    });
+  });
+
+
+
+
+  describe("When I am on NewBill Page and I add an image attached file", () => {
+    test.only("Then the file handler should be run", () => {
+      const newBill = new InitiateNewBill();
+      newBill.method.handleChangeFile = jest.fn()
+      const inputFile = document.querySelector(`input[data-testid="file"]`);
+      fireEvent.change(inputFile, {
+        target: {
+          files: [new File(["sample"], "sample.png", { type: "image/png" })],
+        },
+      });
+      expect(newBill.method.handleChangeFile).toHaveBeenCalled()
+    });
+  });
+
+
+
+
+
+
+  
+  describe("When I am on NewBill Page and I add an image attached file", () => {
+    test("Then the image should be accepted by the app ", () => {
+      new InitiateNewBill();
+      const inputFile = document.querySelector(`input[data-testid="file"]`);
+      fireEvent.change(inputFile, {
+        target: {
+          files: [new File(["sample"], "sample.png", { type: "image/png" })],
+        },
+      });
+      expect(inputFile.files.length).toEqual(1);
+    });
+  });
+  describe("When I am on NewBill Page and I add a non-image attached file", () => {
+    test("Then the image should be rejected by the app ", async () => {
+      new InitiateNewBill();
+      const inputFile = document.querySelector(`input[data-testid="file"]`);
+      fireEvent.change(inputFile, {
+        target: {
+          files: [new File(["sample"], "sample.txt", { type: "text/plain" })],
+        },
+      });
+      //expect(inputFile.files.length).toEqual(0)
+
+      // The test cannot passe successfuly.
+      // to put the file by this way is not an exact simulation of an user's file selection :
+      // the fileInput.value is not modified consequently.
+      // even if a value is passed via object.defineProperty,
+      // the filesList is not cleared in jest environment via the statement inputFile.value= "",
+      // whereas it's working on chrome
+    });
+  });
+});
