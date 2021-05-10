881695381b38e20edf8974bb202e37d95efc9bba
Delile Sebastien <sebastiendelile@gmail.com>
2021-05-10 16:38:57 +0200

    test: update newBill container and newBillUI
    write some test for NewBill class and newBillUI
    some of them are not working yet (commented)

--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -2,6 +2,7 @@ import { fireEvent, screen } from "@testing-library/dom";
 import userEvent from '@testing-library/user-event'
 import NewBillUI from "../views/NewBillUI.js";
 import NewBill from "../containers/NewBill.js";
+import { ROUTES } from "../constants/routes";
 import { localStorageMock } from "../__mocks__/localStorage.js";
 
 //setup for tests
@@ -18,6 +19,8 @@ class InitiateNewBill {
       JSON.stringify({
         type: "Employee",
         email: "a@a",
+        password: "123",
+        status: "connected"
       })
     );
     const html = NewBillUI();
@@ -39,7 +42,7 @@ class FillTheForm {
     date = "2021-05-01",
     vat = "20",
     pct = "20",
-    comment = "YOLO",
+    commentary = "YOLO",
     file = new File(["sample"], "sample.png", { type: "image/png" }),
   } = {}) {
     document.querySelector(`select[data-testid="expense-type"]`).value = type;
@@ -48,10 +51,13 @@ class FillTheForm {
     document.querySelector(`input[data-testid="datepicker"]`).value = date;
     document.querySelector(`input[data-testid="vat"]`).value = vat;
     document.querySelector(`input[data-testid="pct"]`).value = pct;
-    document.querySelector(`textarea[data-testid="commentary"]`).value = comment;
-    Object.defineProperty(document.querySelector(`input[data-testid="file"]`), "files", {
-      value: [file],
-    });
+    document.querySelector(`textarea[data-testid="commentary"]`).value = commentary;
+    const inputFile = document.querySelector(`input[data-testid="file"]`);
+      fireEvent.change(inputFile, {
+        target: {
+          files: [file],
+        },
+      });
   }
 }
 //end of setup
@@ -68,29 +74,71 @@ describe("Given I am connected as an employee", () => {
       expect(form.length).toEqual(9);
     });
   });
-  describe("When I am on NewBill Page and I uncorrectly fill the form and submit(missing type)", () => {
-    test("Then the form should not been sent and the error should be highlighted", () => {
+  describe("When I am on NewBill Page and I correctly fill the form and submit", () => {
+    test("Then the form should be sent with filled data", () => {
       const newBill = new InitiateNewBill();
-      newBill.method.handleSubmit = jest.fn()
+      var submittedBill= {}
+      newBill.method.createBill = jest.fn(function(bill) {
+        for (let key in bill) {
+          submittedBill[key] = bill[key]
+        }
+      })
       new FillTheForm()
       userEvent.click(document.getElementById('btn-send-bill'))
-      expect(newBill.method.handleSubmit).toHaveBeenCalled();
+      expect(newBill.method.createBill).toHaveBeenCalled()
+      expect(submittedBill.email).toEqual("a@a")
+      expect(submittedBill.type).toEqual("Restaurants et bars")
+      expect(submittedBill.name).toEqual("Pot")
+      expect(submittedBill.amount).toEqual(100)
+      expect(submittedBill.date).toEqual("2021-05-01")
+      expect(submittedBill.vat).toEqual("20")
+      expect(submittedBill.pct).toEqual(20)
+      expect(submittedBill.commentary).toEqual("YOLO")
+      //expect(submittedBill.fileURL).toEqual("fakepath/Sample.png")
+      //expect(submittedBill.fileName).toEqual("Sample.png")
+
+      //the fileName & fileURL building occur within a firebase call
     });
+    describe("When I am on NewBill Page and I fill the form whithout pct and submit", () => {
+      test.only("Then the form should be sent with 20 as default value for pct field", () => {
+        const newBill = new InitiateNewBill();
+        var submittedBill= {}
+        newBill.method.createBill = jest.fn(function(bill) {
+          for (let key in bill) {
+            submittedBill[key] = bill[key]
+          }
+        })
+        new FillTheForm({pct: ""})
+        userEvent.click(document.getElementById('btn-send-bill'))
+        expect(submittedBill.pct).toEqual(20)
+      });
+      test("Then the Bills page should be rendered", () => {
+        const newBill = new InitiateNewBill();
+        newBill.method.createBill = jest.fn()
+        new FillTheForm()
+        userEvent.click(document.getElementById('btn-send-bill'))
+        expect(screen.getAllByText("Mes notes de frais")).toBeTruthy()
+      })
+    })
   })
+  /*describe("When I am on NewBill Page and I uncorrectly fill the form and submit(missing type)", () => {
+    test("Then the form should not been sent and the error should be highlighted", () => {
+      const newBill = new InitiateNewBill();
+      var submittedBill= {}
+      newBill.method.createBill = jest.fn(function(bill) {
+        for (let key in bill) {
+          submittedBill[key] = bill[key]
+        }
+      })
+      new FillTheForm({type: ""})
+      userEvent.click(document.getElementById('btn-send-bill'))
+      expect(newBill.method.createBill).not.toHaveBeenCalled();
 
-
-
-  describe("When I am on NewBill Page and I correctly fill the form and submit", () => {
-    test("Then the app should send the info to backend", () => {
-      //yolo
+      //not working, it seems that HTML5 form API is not called, required attribute isn't took into account
     });
-  });
-
-
-
-
-  describe("When I am on NewBill Page and I add an image attached file", () => {
-    test.only("Then the file handler should be run", () => {
+  })*/
+  /*describe("When I am on NewBill Page and I add an image attached file", () => {
+    test("Then the file handler should be run", () => {
       const newBill = new InitiateNewBill();
       newBill.method.handleChangeFile = jest.fn()
       const inputFile = document.querySelector(`input[data-testid="file"]`);
@@ -100,15 +148,10 @@ describe("Given I am connected as an employee", () => {
         },
       });
       expect(newBill.method.handleChangeFile).toHaveBeenCalled()
+      
+      //not working as a function already attached to an eventlistener is not modified by a mock
     });
-  });
-
-
-
-
-
-
-  
+  });*/
   describe("When I am on NewBill Page and I add an image attached file", () => {
     test("Then the image should be accepted by the app ", () => {
       new InitiateNewBill();
@@ -121,7 +164,7 @@ describe("Given I am connected as an employee", () => {
       expect(inputFile.files.length).toEqual(1);
     });
   });
-  describe("When I am on NewBill Page and I add a non-image attached file", () => {
+  /*describe("When I am on NewBill Page and I add a non-image attached file", () => {
     test("Then the image should be rejected by the app ", async () => {
       new InitiateNewBill();
       const inputFile = document.querySelector(`input[data-testid="file"]`);
@@ -130,14 +173,14 @@ describe("Given I am connected as an employee", () => {
           files: [new File(["sample"], "sample.txt", { type: "text/plain" })],
         },
       });
-      //expect(inputFile.files.length).toEqual(0)
+      expect(inputFile.files.length).toEqual(0)
 
-      // The test cannot passe successfuly.
+      // The test cannot pass successfuly.
       // to put the file by this way is not an exact simulation of an user's file selection :
       // the fileInput.value is not modified consequently.
       // even if a value is passed via object.defineProperty,
       // the filesList is not cleared in jest environment via the statement inputFile.value= "",
       // whereas it's working on chrome
     });
-  });
+  });*/
 });
